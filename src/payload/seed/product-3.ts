0d476a4be7b4945d101341b0f647b33646b827e8
Bruno Correia <brunovpm@hotmail.com>
2024-04-21 14:59:00 -0300

    Update product-3.ts

--- src/payload/seed/product-3.ts
+++ src/payload/seed/product-3.ts
@@ -1,4 +1,4 @@
-import type { Product } from '../payload-types'
+import type { Product } from '../payload-types';
 
 export const product3: Partial<Product> = {
   title: 'Online Course',
@@ -7,7 +7,7 @@ export const product3: Partial<Product> = {
   _status: 'published',
   meta: {
     title: 'Online Course',
-    description: 'Make a one-time purchase to gain access to this content',
+    Descrição: 'Make a one-time purchase to gain access to this content',  // Corrigido aqui
     image: '{{PRODUCT_IMAGE}}',
   },
   layout: [
@@ -22,10 +22,6 @@ export const product3: Partial<Product> = {
                 {
                   text: "This content is completely dynamic using custom layout building blocks configured in the CMS. This can be anything you'd like from rich text and images, to highly designed, complex components.",
                 },
-              ],
-            },
-            {
-              children: [
                 {
                   text: 'Purchase this product to gain access to the gated content behind the paywall which will appear below.',
                 },
@@ -40,9 +36,6 @@ export const product3: Partial<Product> = {
         },
       ],
     },
-  ],
-  enablePaywall: true,
-  paywall: [
     {
       blockType: 'content',
       columns: [
@@ -68,9 +61,9 @@ export const product3: Partial<Product> = {
           },
         },
       ],
-    },
+    }
   ],
   relatedProducts: [], // this is populated by the seed script
   priceJSON:
     '{"object":"list","data":[{"id":"price_1NWAmeIsIsxtNwz9bt0Du1VT","object":"price","active":true,"billing_scheme":"per_unit","created":1689913060,"currency":"usd","custom_unit_amount":null,"livemode":false,"lookup_key":null,"metadata":{},"nickname":null,"product":"prod_OImLJbmlbfwzM0","recurring":null,"tax_behavior":"unspecified","tiers_mode":null,"transform_quantity":null,"type":"one_time","unit_amount":4999,"unit_amount_decimal":"4999"}],"has_more":false,"url":"/v1/prices"}',
-}
+};
