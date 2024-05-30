db5dd817337488717b31788f581f151e8c218cad
Bruno Correia <brunovpm@hotmail.com>
2024-05-30 12:39:09 -0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  "extends": "next",
-  "rules": {
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
-  }
-}
+  root: true,
+  extends: ['plugin:@next/next/recommended', '@payloadcms'],
+  ignorePatterns: ['**/payload-types.ts'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 'off',
+  },
