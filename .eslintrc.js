a507c638dfa946401aaee474b9a93fa4478ce98a
Bruno Correia <brunovpm@hotmail.com>
2024-05-30 12:33:16 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,7 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@next/next/recommended',
-    '@payloadcms',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier', '@typescript-eslint'],
-  rules: {
-    'prettier/prettier': ['warn', { 
-      semi: false, 
-      endOfLine: 'auto',
-      requirePragma: false,
-      insertPragma: false,
-      singleQuote: true,
-      trailingComma: 'all'
-    }],
-    'no-console': 'off',
-    '@typescript-eslint/ban-ts-comment': [
-      'warn',
-      {
-        'ts-expect-error': 'allow-with-description'
-      }
-    ]
-  },
+  "extends": "next",
+  "rules": {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
 }
