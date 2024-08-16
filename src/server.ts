c8e4bb16f55bd92ec51fbd13babc3be3a5e72e26
Bruno Correia <brunovpm@hotmail.com>
2024-08-16 10:28:34 -0300

    linter

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,25 @@
-import CancelFreightRouter from './payload/endpoints/melhor_envio_cancelamento'
-import CheckoutFreightRouter from './payload/endpoints/melhor_envio_checkout'
-import EmailRouter from './payload/endpoints/email_compra'
-import EmailRouterCad from './payload/endpoints/email_cadastro'
-import GeraEtiquetaFreightRouter from './payload/endpoints/melhor_envio_add_etiqueta'
-import NotaFiscal from './payload/endpoints/nota_fiscal'
-import PrintEtiquetaFreightRouter from './payload/endpoints/melhor_envio_print_etiqueta'
-import calculateFreightRouter from './payload/endpoints/melhor_envio'
-import carrinhoFreightRouter from './payload/endpoints/melhor_envio_add_carrinho'
 import dotenv from 'dotenv'
 import express from 'express'
 import next from 'next'
 import nextBuild from 'next/dist/build'
-import orderHandler from './payload/endpoints/order-handler'
 import path from 'path'
 import payload from 'payload'
+
+import EmailRouterCad from './payload/endpoints/email_cadastro'
+import EmailRouter from './payload/endpoints/email_compra'
 import processPayment from './payload/endpoints/gateway_pagamento'
+import calculateFreightRouter from './payload/endpoints/melhor_envio'
+import carrinhoFreightRouter from './payload/endpoints/melhor_envio_add_carrinho'
+import GeraEtiquetaFreightRouter from './payload/endpoints/melhor_envio_add_etiqueta'
+import CancelFreightRouter from './payload/endpoints/melhor_envio_cancelamento'
+import CheckoutFreightRouter from './payload/endpoints/melhor_envio_checkout'
+import PrintEtiquetaFreightRouter from './payload/endpoints/melhor_envio_print_etiqueta'
+import NotaFiscal from './payload/endpoints/nota_fiscal'
+import orderHandler from './payload/endpoints/order-handler'
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const app = express()
 const PORT = process.env.PORT || 3000
 
