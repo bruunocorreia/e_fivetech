aa32b78408aeb9f7dde429983921ed4a3025118e
Bruno Correia <brunovpm@hotmail.com>
2024-10-01 19:00:26 -0300

    delete live preview

--- src/app/(pages)/products/page.client.tsx
+++ src/app/(pages)/products/page.client.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { useLivePreview } from '@payloadcms/live-preview-react'
-
 import { Gutter } from '../../../app/_components/Gutter'
 import RichText from '../../../app/_components/RichText'
 import { Page } from '../../../payload/payload-types'
@@ -10,11 +8,7 @@ import { Blocks } from '../../_components/Blocks'
 import classes from './index.module.scss'
 
 export const PageTemplate: React.FC<{ page: Page | null | undefined }> = ({ page }) => {
-  const { data } = useLivePreview({
-    serverURL: 'http://localhost:3000/products',
-    depth: 2,
-    initialData: page,
-  })
+  const data = page
 
   return (
     <Gutter>
