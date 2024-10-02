09ad31e094e94d09fcfc70e5d6a28ee5a3731262
Bruno Correia <brunovpm@hotmail.com>
2024-10-02 10:19:59 -0300

    add nota fiscal

---
+++ src/app/_components/NotaFiscal/index.tsx
@@ -0,0 +1,123 @@
+'use client'
+
+import React, { Fragment, useCallback, useEffect, useState } from 'react'
+
+import { Button } from '../Button'
+import { Input } from '../Input'
+import { Message } from '../Message'
+import axios from 'axios'
+import classes from './index.module.scss'
+import { useAuth } from '../../_providers/Auth'
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+  ref: string
+}
+
+export const NFeCreator = ({ onNFeCreation, onRefChange }) => {
+  const [nfeInfo, setNfeInfo] = useState(null)
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState('')
+  const { user, setUser } = useAuth()
+
+  const ref = user?.ref || null
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isLoading },
+    reset,
+    watch,
+  } = useForm<FormData>()
+
+  useEffect(() => {
+    if (ref) {
+      reset({ ref })
+      createNFe({ ref })
+    }
+  }, [ref, reset])
+
+  const createNFe = useCallback(
+    async data => {
+      const { ref } = data
+      onRefChange(ref)
+
+      setError('')
+      setNfeInfo(null)
+
+      try {
+        const response = await axios.post('/api/enviar-nfe', { ref })
+        const nfeResponse = response.data
+
+        const formattedNfeInfo = {
+          price: parseFloat(nfeResponse.valor_total),
+          deliveryTime: `${nfeResponse.data_entrada_saida}`,
+          serviceId: nfeResponse.ref,
+        }
+
+        setNfeInfo(formattedNfeInfo)
+        onNFeCreation(formattedNfeInfo.price, formattedNfeInfo.serviceId)
+        if (!ref) {
+          await updateUserRef(ref)
+        }
+      } catch (err) {
+        console.error('Erro ao criar NF-e:', err)
+        setError('Falha ao criar a NF-e. Tente novamente.')
+        onNFeCreation(0, null)
+      }
+    },
+    [onNFeCreation, ref],
+  )
+
+  const updateUserRef = useCallback(
+    async ref => {
+      if (user && !ref && ref !== ref) {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/${user.id}`, {
+          credentials: 'include',
+          method: 'PATCH',
+          body: JSON.stringify({ ref }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if (response.ok) {
+          const json = await response.json()
+          setUser(json.doc)
+          setError('')
+        } else {
+          setError('There was a problem updating your account.')
+        }
+      }
+    },
+    [user, setUser],
+  )
+
+  return (
+    <form onSubmit={handleSubmit(createNFe)} className={classes.form}>
+      <Message error={error} success={success} className={classes.message} />
+      {!ref && (
+        <Fragment>
+          <Input name="ref" label="Referência da Nota" register={register} type="text" />
+          <Button
+            type="submit"
+            label={isLoading ? 'Criando...' : 'Criar Nota Fiscal'}
+            disabled={isLoading}
+            appearance="primary"
+            className={classes.submit}
+          />
+        </Fragment>
+      )}
+      {nfeInfo && ref && (
+        <div className={classes.result}>
+          <b>
+            <p>Valor Total: R$ {nfeInfo.price}</p>
+          </b>
+          <p>Data de Emissão: {nfeInfo.deliveryTime}</p>
+        </div>
+      )}
+    </form>
+  )
+}
+
+export default NFeCreator
---
+++ src/payload/endpoints/nota_fiscal.ts
@@ -0,0 +1,80 @@
+import { Router } from 'express'
+import axios from 'axios'
+
+const router = Router()
+
+router.post('/enviar-nfe', async (req, res) => {
+  const ref = '12345' // Atribuição correta da variável ref
+  const token = process.env.NOTA_FISCAL // Atribuição correta do token
+
+  try {
+    const response = await axios.post(
+      `https://homologacao.focusnfe.com.br/v2/nfe?ref=${ref}`,
+      {
+        natureza_operacao: 'Remessa',
+        data_emissao: '2018-03-21T11:00:00',
+        data_entrada_saida: '2018-03-21T11:00:00',
+        tipo_documento: '1',
+        finalidade_emissao: '1',
+        cnpj_emitente: '51916585000125',
+        nome_emitente: 'ACME LTDA',
+        nome_fantasia_emitente: 'ACME LTDA',
+        logradouro_emitente: 'R. Padre Natal Pigato',
+        numero_emitente: '100',
+        bairro_emitente: 'Santa Felicidade',
+        municipio_emitente: 'Curitiba',
+        uf_emitente: 'PR',
+        cep_emitente: '82320030',
+        inscricao_estadual_emitente: '1234567',
+        nome_destinatario: 'NF-E EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL',
+        cpf_destinatario: '51966818092',
+        telefone_destinatario: '1196185555',
+        logradouro_destinatario: 'Rua São Januário',
+        numero_destinatario: '99',
+        bairro_destinatario: 'Crespo',
+        municipio_destinatario: 'Manaus',
+        uf_destinatario: 'AM',
+        pais_destinatario: 'Brasil',
+        cep_destinatario: '69073178',
+        valor_frete: '0.0',
+        valor_seguro: '0',
+        valor_total: '47.23',
+        valor_produtos: '47.23',
+        modalidade_frete: '0',
+        items: [
+          {
+            numero_item: '1',
+            codigo_produto: '1232',
+            descricao: 'Cartões de Visita',
+            cfop: '6923',
+            unidade_comercial: 'un',
+            quantidade_comercial: '100',
+            valor_unitario_comercial: '0.4723',
+            valor_unitario_tributavel: '0.4723',
+            unidade_tributavel: 'un',
+            codigo_ncm: '49111090',
+            quantidade_tributavel: '100',
+            valor_bruto: '47.23',
+            icms_situacao_tributaria: '400',
+            icms_origem: '0',
+            pis_situacao_tributaria: '07',
+            cofins_situacao_tributaria: '07',
+          },
+        ],
+      },
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+    res.json(response.data)
+  } catch (error) {
+    console.error('Erro ao enviar NFe:', error)
+    res.status(500).send('Falha ao enviar NFe. Por favor, tente novamente.')
+  }
+})
+
+export default router
