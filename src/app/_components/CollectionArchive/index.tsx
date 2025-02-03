96d2927af067551acc3564675cb9197d959f541e
Bruno Correia <brunovpm@hotmail.com>
2025-02-02 21:24:41 -0300

    fix exibir produtos mesmo sem estoque

--- src/app/_components/CollectionArchive/index.tsx
+++ src/app/_components/CollectionArchive/index.tsx
@@ -180,13 +180,13 @@ export const CollectionArchive: React.FC<Props> = props => {
 
             ...(whereConditions.length > 0 ? { or: whereConditions } : {}),
 
-            or: [
-              { 'stock.PP': { greater_than: 0 } },
-              { 'stock.P': { greater_than: 0 } },
-              { 'stock.M': { greater_than: 0 } },
-              { 'stock.G': { greater_than: 0 } },
-              { 'stock.GG': { greater_than: 0 } },
-            ],
+            // or: [
+            //   { 'stock.PP': { greater_than: 0 } },
+            //   { 'stock.P': { greater_than: 0 } },
+            //   { 'stock.M': { greater_than: 0 } },
+            //   { 'stock.G': { greater_than: 0 } },
+            //   { 'stock.GG': { greater_than: 0 } },
+            // ],
           },
           limit,
           page,
